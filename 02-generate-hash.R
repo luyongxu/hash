a60cc8aa0bed8354ee31326432005cca22b51995
Coke <luyongxu@gmail.com>
2017-12-27 13:01:37 -0500

    Add length argument

--- 02-generate-hash.R
+++ 02-generate-hash.R
@@ -25,10 +25,11 @@ source("./01-load-packages.R")
 #' algorithm: An openssl hashing function that indicates the cryptographic hash function.  
 #' rounds: An integer indicating the number of hashing rounds.  
 #' salt: A character string that is concatenated to the string.  
+#' length: An integer indicating the length of the desired hash.  
 #' 
 #' Value  
 #' Returns a string indicating the cryptographic hash.  
-generate_hash <- function(x, algorithm, rounds, salt) { 
+generate_hash <- function(x, algorithm, rounds, salt, length = 64) { 
   
   # Append salt 
   string <- str_c(x, salt)
@@ -44,6 +45,9 @@ generate_hash <- function(x, algorithm, rounds, salt) {
   # Modify hash
   str_sub(hash, 5, 5) <- "@"
   str_sub(hash, 8, 8) <- "O"
+  
+  # Truncate hash
+  hash <- str_sub(hash, 1, length)
   
   # Return hash
   return(hash)
